6dbb7ba144c4fbcf4482deb8e627849a1e857f30
LeeHyewon <hwlee@okpos.co.kr>
2022-05-10 16:21:49 +0900

    설정 파일 추가 및 일부 파일들 수정

--- src/main/java/org/example/repository/TodoRepository.java
+++ src/main/java/org/example/repository/TodoRepository.java
@@ -4,6 +4,15 @@ import org.example.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+    public List<TodoEntity> findByOrder(Long order);
+
+    public List<TodoEntity> findByTitleContaining(String title);
+
+    public List<TodoEntity> findByCompletedOrderByOrderDesc(boolean completed);
+
+    public List<TodoEntity> findByIdGreaterThanAndTitleContaining(Long id, String title);
 }
